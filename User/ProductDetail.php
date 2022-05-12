2b3504cf7becb334973b48014177a3ce46c446ee
quachhoang2002 <97105329+quachhoang2002@users.noreply.github.com>
2022-05-12 15:55:15 +0700

    update

--- User/ProductDetail.php
+++ User/ProductDetail.php
@@ -6,34 +6,61 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="../jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="../bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="font/themify-icons/themify-icons.css">
+    <script src="../bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+    <script   src="../jquery-3.6.0.min.js"></script>
 </head> 
   <?php  
     require '../connect.php';
    $id=$_GET['id'];
-   $sql="select*from product 
-          where id=$id ";
+   $sql="select  product.*,category.Type,manufacture.name from product
+          join category on product.product_type =category.id
+          join manufacture on manufacture_id=manufacture.Ma
+          where product.id=$id ";
     $result=mysqli_query($connect,$sql);
     $content=mysqli_fetch_array($result);
   
    ?>
+   <?php require 'header.php' ?>
 <body> 
-     <div> 
-           <img src="../admin/product/photos/<?php  echo $content['Image'] ?>" alt="" srcset="" height="100px" >
+  <div class="container-fluid pt-5 pb-5" style="background-color:aliceblue">
+      <div class="row d-flex justify-content-center">
+          <div class="col-4">
+              <img src="../admin/product/photos/<?php echo $content['Image'] ?>" alt="" srcset="" style="width:500px;height:600px ">
+          </div>
+          <div class="col-4 mt-5 text-center">
           <div id="name"> <?php echo $content['Name'] ?> </div>
-          <div id="price"> <?php echo $content['Price'] ?>  </div>
-          <div > <?php echo $content['product_type'] ?>  </div>
-          <div> <?php echo $content['description'] ?>  </div>
-          <div> <?php echo $content['manufacture_id'] ?>  </div>
-          <input type="number" value="1" name="quantity" min="1" max="50"   onchange="checkvalue()" > 
+            <label>   <?php echo $content['Type'] ?></label>
+            <hr>
+             <span>              
+                 <span class="fa fa-star checked"></span>
+                 <span class="fa fa-star checked"></span>
+                 <span class="fa fa-star checked"></span>
+                 <span class="fa fa-star"></span>
+                 <span class="fa fa-star"></span>
+                 <p>Da ban:</p>
+             </span>
+             
+            <p style="font-size:20px" class="p-3 bg-light">  <?php echo $content['Price'] ?> </p>
+            <p style="font-size:20px;text-align:center" > <?php echo $content['description'] ?> </p>  
+            <p style="font-size:20px;text-align: center;">NSX:<?php echo $content['name'] ?></p>
+            <p><input type="number" value="1" name="quantity" min="1" max="50"   onchange="checkvalue()" >  </p>
+
+             <input type="hidden" name="image" value="<?php echo $content['Image'] ?>" >
+             <input type="hidden" name="user_id" value="<?php if(isset($_SESSION['id'])) echo $_SESSION['id']; else echo "" ;?>" >           
+             <input type="button" class="btn btn-primary"  onclick="addtoCart(<?php echo $content['id'] ?>)" value="addtoCart"> 
+             <input type="button" class="btn btn-primary" onclick="order(<?php echo $content['id'] ?>)" value="order"> 
+
+             
        
-          <input type="hidden" name="image" value="<?php echo $content['Image'] ?>" >
-           <input type="hidden" name="user_id" value="<?php if(isset($_SESSION['id'])) echo $_SESSION['id']; else echo "" ;?>" >
-            
-           <input type="button" onclick="addtoCart(<?php echo $content['id'] ?>)" value="addtoCart"> 
-           <input type="button" onclick="order(<?php echo $content['id'] ?>)" value="order"> 
-     </div>
+          </div>
+      </div>
+   </div>
+
 </body>
+<?php require 'footer.php' ?>
 
  <script type="text/javascript"> 
      
--- login/login-form.php
+++ login/login-form.php
@@ -6,6 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="font/themify-icons/themify-icons.css">
+    <script src="../bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+    <script   src="../jquery-3.6.0.min.js"></script>
 </head>
 <?php  
  
@@ -27,22 +32,61 @@ if(isset($_COOKIE['remember'])){
 
  ?>
 <body>
-    <a href="register-form.php">Dang Ky</a>
-      <form action="process.php?action=login" method="POST">
-         Ten dang nhap <input type="text" name="username"> 
-         Mat khau <input type="password" name="password" >
-         Ghi nho dang nhap <input type="checkbox" name="remember">
-         <button>dang nhap </button>
-         <?php if(isset($_SESSION['error'])){
-             echo $_SESSION['error'];
-             unset($_SESSION['error']);
-         } 
-         
-         
-         ?>
+   
 
-
-      </form>
+ <section class="vh-100 gradient-custom">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div class="card bg-dark text-white" style="border-radius: 1rem;">
+            <div class="card-body p-5 text-center">
+  
+              <div class="mb-md-5 mt-md-4 pb-5">
+  
+                <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                <p class="text-white-50 mb-5">Please enter your login and password!</p>
+ 
+             <form action="process.php?action=login" method="POST">
+    
+                
+                 <div class="form-outline form-white mb-4">
+                  <label class="form-label" for="Username">Username</label>
+                  <input type="text" name="username" id="Username" class="form-control form-control-lg" placeholder="Username" />  
+                </div>
+  
+                <div class="form-outline form-white mb-4">
+                  <label class="form-label" for="typePasswordX">Password</label>
+                  <input type="password" name="password" id="typePasswordX" class="form-control form-control-lg"   placeholder="password" />
+                </div>
+                Ghi nho dang nhap <input type="checkbox" name="remember">
+                <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+  
+                <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+             </form>
+                
+  
+                <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                  <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                  <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                  <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                </div>
+  
+              </div>
+                         <?php if(isset($_SESSION['error'])) : ?>
+                            <span class="bg-danger"> <?php echo $_SESSION['error'];unset($_SESSION['error']) ?> </span>
+                         <?php  endif ?>
+               <div>
+                <p class="mb-0">Don't have an account? <a href="register-form.php" class="text-white-50 fw-bold">  Dang Ky</a>
+                </p>
+              </div>
+         
+  
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
 
 </body>
